7d83e2d00872926c076e38041ced75e330230672
fangdol888 <fangdol888@gmail.com>
2019-08-12 20:24:11 +0900

    initiating
    Signed-off-by: fangdol888 <fangdol888@gmail.com>

---
+++ Player.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Shooting{
+	public class Player(){
+		
+	}
+}
+
---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Shooting{
+	public class Program{
+		public static void Main(string[] args){
+			
+		}
+	}
+}
